d34a89593201467294985631fabe81412184fbc0
Evan Lefaure <evlefaure@univ-lyon2.fr>
2023-12-04 15:12:10 +0100

    fichier testpoker (prototype)

---
+++ testpoker.py
@@ -0,0 +1,29 @@
+"""
+@poker.py
+@the application allows players to play a game of planning poker
+"""
+
+
+class Poker:
+    """
+    @class Poker
+    @desc class
+
+    @param online: bool: determin if the poker is online or local
+    @param tab: a tab containing all the available cards to play in the poker
+    """
+    online = False
+    tab = {"0", "1", "2", "3", "5", "8", "13", "20", "40", "100", "cafe", "interro"}
+    pseudo =""
+    mode = 0
+    cpt = "0"
+
+    def init():
+        nbjoueur = input("enter the number of players")
+        print(nbjoueur)
+
+        for x in range(nbjoueur):
+            cpt = cpt + 1
+            print("joueur", cpt, "")
+
+    
